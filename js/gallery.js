459e28fb4ca9066eb452c299999a1a2595f7b273
Noah Doucette <noahd2224@gmail.com>
2025-05-29 13:07:25 -0700

    added setImages function

--- js/gallery.js
+++ js/gallery.js
@@ -7,3 +7,14 @@ function unDo(){
   document.getElementById('image').style.backgroundImage = "none";
   document.getElementById('image').innerHTML = "Hover over an image below to display here.";
 }
+
+function setImages(){
+  options = ["cliff_plants.jpg","dry_hillside_trail.jpg","rainbow_beach.jpg","shaded_trail.jpg","stone_stairs.jpg","sunny_pines.jpg"];
+  currentImages = document.querySelectorAll(".flex img");
+  for(var i=0; i<currentImages.length; i++){
+    console.log("Image "+i);
+    randomImg = "images/"+options[Math.floor(Math.random()*options.length)];
+    currentImages[i].src = randomImg;
+    currentImages[i].setAttribute("tabindex","0");
+  }
+}
